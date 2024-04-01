52014a9add1054e16bfc7d5c276a9f2c048362a4
Adhwa Moyafi Hartoyo <adhwamoyafi.h003@gmail.com>
2024-04-01 13:31:26 +0700

    Semua Tugas

---
+++ LampuApill/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+int redLED = D1;
+int yellowLED = D2;
+int greenLED = D3;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(redLED,HIGH);
+  delay(2000);
+  digitalWrite(redLED,LOW);
+
+  digitalWrite(yellowLED,HIGH);
+  delay(500);
+  digitalWrite(yellowLED,LOW);
+
+  digitalWrite(greenLED,HIGH);
+  delay(3000);
+  digitalWrite(greenLED,LOW);
+}
