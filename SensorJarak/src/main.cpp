3252bcd77db5c8734690eca07dc18016a0da7917
Adhwa Moyafi Hartoyo <adhwamoyafi.h003@gmail.com>
2024-04-01 14:19:10 +0700

    Pertemuan 3

--- SensorJarak/src/main.cpp
+++ SensorJarak/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int trigPin = D1;
+int echoPin = D0;
+int led = D2;
+
+long duration;
+long distance;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(led, OUTPUT);
+  pinMode(trigPin,OUTPUT );
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = 0.034 * duration/2;
+  Serial.print("Jarak: ");
+  Serial.print(distance);
+  Serial.print(" cm\n");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(distance <=10){
+    digitalWrite(led, HIGH);
+  }else{
+    digitalWrite(led, LOW);
+  }
+
+  delay(1000);
 }
